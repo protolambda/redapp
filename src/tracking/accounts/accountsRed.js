d6cc1a772e1717fc1ce0ba3b24f4684871f8bd59
protolambda <proto@protolambda.com>
2018-07-12 15:38:00 +0200

    Accounts reducer

---
+++ src/tracking/accounts/accountsRed.js
@@ -0,0 +1,32 @@
+import accountsAT from './accountsAT';
+import mappedReducer from '../../mapped-reducer';
+
+const initialState = {
+  accountsMap: {}
+  // TODO also track which of these is the default account?
+};
+
+const mapping = {
+  [accountsAT.ACCOUNTS_FETCH_COMPLETED]: (state, action) => ({
+    ...state,
+    accountsMap: action.accountsMap
+  }),
+  [accountsAT.ACCOUNTS_SYNC_COMPLETED]: (state, action) => {
+    const newAccMap = state.accountsMap;
+
+    for (const key of action.accountsMap) {
+      const value = action.accountsMap[key];
+      // Merge in new updated data.
+      newAccMap[key] = {
+        ...newAccMap[key],
+        value
+      };
+    }
+    return ({
+      ...state,
+      accountsMap: state.accountsMap
+    });
+  }
+};
+
+export default mappedReducer(mapping, initialState);
