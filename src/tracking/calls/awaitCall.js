4fccd2bae56aad6d1536d8f484e9e6e27cb44a78
protolambda <proto@protolambda.com>
2018-07-19 15:49:26 +0200

    Make call tracking use yield-call pattern, instead of direct await

--- src/tracking/calls/awaitCall.js
+++ src/tracking/calls/awaitCall.js
@@ -1,4 +1,4 @@
-import {put} from 'redux-saga/effects';
+import {call, put} from 'redux-saga/effects';
 import callsAT from './callsAT';
 
 /**
@@ -8,16 +8,21 @@ import callsAT from './callsAT';
  * @param callID The ID of the call, used in the caching system.
  * @returns {Promise} The promise, complete when the given callPromise is processed.
  */
-const awaitCall = async (callPromise, callID) => {
+function* awaitCall(callPromise, callID) {
   try {
     // TODO: should we apply a time-out here, or does the web3 provider stop it anyway?
-    const callResult = await callPromise;
+
+    // Note: the outer "call" being made here is a redux-saga call that makes this generator
+    //  effectively await the promise, without taking away the power of orchestration
+    //  (what "await ..." would do)
+    // The inner call is the Web3 contract call, returning a promise.
+    const callResult = yield call(callPromise);
 
     put({type: callsAT.CALL_RETURNED, callID, value: callResult});
   } catch (err) {
     // Handle failure, forward error, user can check what happened.
     put({type: callsAT.CALL_FAILED, callID, err});
   }
-};
+}
 
 export default awaitCall;
