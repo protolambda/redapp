4d0eec2ff9d18fbda34f6cf8e1b849d7ba1e954b
protolambda <proto@protolambda.com>
2018-07-20 13:38:07 +0200

    Fix reducer linking

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import web3Red from './web3/web3Red';
 import trackingRed from './tracking/trackingRed';
 import contractsRed from './contracts/contractsRed';
 
 const reducer = combineReducers({
-  web3: web3Red,
   tracking: trackingRed,
   contracts: contractsRed
 });
--- src/tracking/trackingRed.js
+++ src/tracking/trackingRed.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import accountsRed from './accounts/accountsRed';
 import blocksRed from './blocks/blocksRed';
 import transactionsRed from './transactions/transactionsRed';
+import callsRed from './calls/callsRed';
 
 const trackingRed = combineReducers({
   accounts: accountsRed,
   blocks: blocksRed,
-  transactions: transactionsRed
+  transactions: transactionsRed,
+  calls: callsRed
 });
 
 export default trackingRed;
