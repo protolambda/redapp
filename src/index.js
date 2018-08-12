6ff18877ac4698a1259e02aac10bb6b04504286b
protolambda <proto@protolambda.com>
2018-08-12 18:23:50 +0200

    Add action modules to main index

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import initWeb3 from './initWeb3';
 import contractsAT from './contracts/contractsAT';
 import contractsRed from './contracts/contractsRed';
 import contractsSaga from './contracts/contractsSaga';
-const contracts = {contractsAT, contractsRed, contractsSaga};
+import contractsActions from './contracts/contracts';
+const contracts = {contractsAT, contractsRed, contractsSaga, contracts: contractsActions};
 
 
 // Tracking
@@ -23,7 +24,8 @@ const contracts = {contractsAT, contractsRed, contractsSaga};
 import accountsAT from './tracking/accounts/accountsAT';
 import accountsRed from './tracking/accounts/accountsRed';
 import accountsSaga from './tracking/accounts/accountsSaga';
-const accounts = {accountsAT, accountsRed, accountsSaga};
+import accountsActions from './tracking/accounts/accounts';
+const accounts = {accountsAT, accountsRed, accountsSaga, accounts: accountsActions};
 
 import blocksAT from './tracking/blocks/blocksAT';
 import blocksRed from './tracking/blocks/blocksRed';
@@ -33,12 +35,15 @@ const blocks = {blocksAT, blocksRed, blocksSaga};
 import callsAT from './tracking/calls/callsAT';
 import callsRed from './tracking/calls/callsRed';
 import callsSaga from './tracking/calls/callsSaga';
-const calls = {callsAT, callsRed, callsSaga};
+import callsActions from './tracking/calls/calls';
+const calls = {callsAT, callsRed, callsSaga, calls: callsActions};
 
 import transactionsAT from './tracking/transactions/transactionsAT';
 import transactionsRed from './tracking/transactions/transactionsRed';
 import transactionsSaga from './tracking/transactions/transactionsSaga';
-const transactions = {transactionsAT, transactionsRed, transactionsSaga};
+import transactionsActions from './tracking/transactions/transactions';
+const transactions = {transactionsAT, transactionsRed, transactionsSaga,
+  transactions: transactionsActions};
 
 const tracking = {accounts, blocks, calls, transactions};
 
